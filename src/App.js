6a32c5acdf4c20cb5b833f001dedd0d95a1d6888
manmakris <manolis.makris@datawise.ai>
2023-03-27 18:23:58 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import * as ml5 from "ml5";
+
+import "./App.css";
+const sounds = [
+  {label:'Heey',translation:'Που είσαι χαμένος;'},
+  {label:'come_here',translation:'Έλα να σου πω.'},
+  {label:'Hey_Whistle',translation:'Που είσαι χαμένος;'},
+  {label:'Come_her_whistle',translation:'Έλα να σου πω.'},
+]
 
 function App() {
+  const [label, setLabel] = useState("");
+  const [isLoading,setIsLoading] = useState(true)
+  const classifierRef = useRef(null);
+
+  const classifySound = async (p) => {
+    const options = {
+      probabilityThreshold: 0.65,
+      // audioContext: p.getAudioContext(),
+    };
+    classifierRef.current = await ml5.soundClassifier(
+      `https://storage.googleapis.com/tm-model/JD8fWwrxe/model.json`,
+      options,
+      onModelReady
+    );
+
+    function onModelReady() {
+      console.log("Model is ready");
+      setIsLoading(false)
+      classifierRef.current.classify(onResult);
+    }
+
+    function onResult(error, results) {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      if (results[0].confidence >= 0.95 && results[0].label !=='Background Noise' ) {
+        const findTranslation = sounds.find((f)=>f.label === results[0].label)
+        if (findTranslation)
+        {setLabel(findTranslation.translation);
+        classifierRef.current.classify(onResult);}
+      }
+    }
+  };
+
+  useEffect(() => {
+    classifySound();
+  }, []);
+
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isLoading?
+        <>
+          <h1>Φορτώνει...</h1>
+        </>  
+        :
+        <>
+        <h1>Μεταφραστής Λέρικων σφυριγμάτων</h1>
+        <h6>Μετάφραση:</h6>
+        <p>{label}</p>
+        </>
+      }
+        
       </header>
     </div>
   );
